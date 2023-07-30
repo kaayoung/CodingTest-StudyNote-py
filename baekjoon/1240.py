b2ef23e6d5d4e19cb66cdff70390a37351bf1660
kaayoung <87481901+kaayoung@users.noreply.github.com>
2023-07-30 15:29:06 +0900

    fix 1240

--- baekjoon/1240.py
+++ baekjoon/1240.py
@@ -1,8 +1,6 @@
 # 1240
 
 import sys
-sys.setrecursionlimit(10**6)
-
 n,m = map(int, sys.stdin.readline().split())
 
 graph = [[0]*(n+1) for _ in range(n+1)]
@@ -21,7 +19,6 @@ def dfs(start , end , distance) : # start:시작하는 노드 / end:최종적으
         if graph[start][i] >0 and not visited[i] :
             if end == i :
                 result = distance + graph[start][i]
-                return
             else :
                 dfs(i,end,distance+graph[start][i])
 
@@ -31,4 +28,3 @@ for _ in range(m) :
     visited = [False] * (n+1)
     dfs(x,y,0)
     print(result)
-
